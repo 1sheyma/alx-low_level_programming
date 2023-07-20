5b9cb011c0ea4ed985a319e15d000520c78ef36b
1sheyma <sheymitari98@gmail.com>
2023-07-20 08:16:41 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#ifndef MAIN_H
+#define MAIN_H
 
-/* betty style doc for function main goes there */
-int main(void)
-{
-	int n;
+#include <stdio.h>
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
